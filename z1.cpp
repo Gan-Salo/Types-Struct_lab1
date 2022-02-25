4ac9a0c092d0af17c0c5203da6d57109c8fe514d
Gan-Salo <90299377+Gan-Salo@users.noreply.github.com>
2022-02-25 21:51:20 +0700

    Добавлены: 1)простая и 2)быстрая сортировка, 3)Бинарный поиск

--- z1.cpp
+++ z1.cpp
@@ -2,8 +2,10 @@
 //
 
 #include <iostream>
-#include <time.h>
+#include <cstdlib>
+#include <ctime>
 
+using namespace std;
 
  /*Сортировка вставками*/
 void floatsort(float a[], int n)
@@ -35,49 +37,78 @@ void Quick_Sort(int left, int right, float a[]) {
 
     m = a[(i + j + 1) / 2];
     do {
-        while (a[i] < m) i++; while (a[j] > m) j--; if (i <= j) {
+        while (a[i] > m) i++; while (a[j] < m) j--; if (i <= j) {
             Exchange(i, j, a); //обмен i, j элементов местами
             i++; j--;
         }
     } while (i <= j);
     if (left < j)
-        Quick_Sort(left, j, a); if (i < right)
+        Quick_Sort(left, j, a); 
+    if (i < right)
         Quick_Sort(i, right, a);
 }
 
-void Bin_Search(int left, int right, float a[]) {
+void SearchRez(float a[], bool flag, int m) {
 
-    int i, j, h;
-    float m;
+    if (flag) cout << "\nЭлемент массива " << a[m] << " расположен в ячейке под индексом " << m << "\n";
+    else cout << "\nТакого элемента нет в массиве...\n";
+}
+
+void Bin_Search(int left, int right, float a[], float find) {
+
+    int i, j, m;
+    
     bool flag = false;
     i = left;
     j = right;
-    while ((i <= j) && (!flag))
+    while ((i <= j) && (flag != true))
     {
 
-    }
-
-    m = a[(i + j + 1) / 2];
+        m = (i + j) / 2;
+        if (a[m] == find) { flag = true; }
+        if (a[m] < find) { j = m - 1; }
+        else { i = m + 1; }
+    }   
     
+    SearchRez(a, flag, m);
+
 }
 
 int main()
 {
-      
-    float *mass, x;
+    setlocale(LC_ALL, "rus");
+    float *mass, find;
     int size;
-    std::cin >> size;
+    cin >> size;
+
     mass = new float[size];
     int i, j;
     
+    srand((unsigned int)time(NULL));
+    float x = 5.0;
     for (i = 0; i < size; i++)
-        std::cin >> mass[i];
-
-    //floatsort(mass, size);
-
+    {
+        cin >> mass[i];
+        //mass[i] = (float(rand()) / float((RAND_MAX)) * x);
+        //cout << mass[i] << "\n";
+    }
+    /*std::
+    //floatsort(mass, size);*/
+    
     Quick_Sort(0, size - 1, mass);
+    cout << "\nРезультат сортировки по убыванию:\n";
     for (i = 0; i < size; i++)
-        std::cout << mass[i] << " ";
+        cout << mass[i] << " \n";
+    
+
+    std::cout << "\nКакое число в массиве требуется найти: ";
+    std::cin >> find;
+
+    
+    cout << "\nРезультат поиска:";
+    Bin_Search(0, size - 1, mass, find);
+
+    
 
     delete[] mass;
     mass = 0;
